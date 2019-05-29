69257a39a2bc34660532081f6ff1a51c517bdfa2
Craig Buckler <craig@optimalworks.net>
2019-05-29 17:48:41 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,125 @@
 # typist.js
-Web page typing effect animation.
+
+typist.js shows a typing effect which can be applied to any HTML element or parent element.
+
+Please use the code as you wish - [tweet me @craigbuckler](https://twitter.com/craigbuckler) if you find it useful.
+
+* lightweight: 2,600 bytes of JavaScript, 245 bytes of CSS
+* no external dependencies - works with any framework
+* easy to configure from HTML or JavaScript
+* works in all modern browsers (IE11+)
+* progressively-enhanced to avoid failure in older browsers.
+
+
+## Basic example
+
+The page must load the CSS and JavaScript. It can be placed anywhere on the page but, typically, the CSS is loaded in the `<head>` and the JS is loaded just before the closing `</body>` tag:
+
+```html
+<link rel="stylesheet" href="dist/typist.css">
+<script src="dist/typist.js"></script>
+```
+
+*(A module bundler can also be used.)*
+
+To apply a typing effect to any element, add a `data-typist` attribute:
+
+```html
+<p data-typist="">Type and retype this paragraph forever!</p>
+```
+
+Pass a numeric value to the attribute to retype that number of times:
+
+```html
+<p data-typist="1">This paragraph is typed once.</p>
+```
+
+
+## Multiple elements
+
+When an element has two or more child elements, their text is typed in sequence:
+
+```html
+<p data-typist="">
+  <span>This is typed first.</span>
+  <span>This is typed second.</span>
+  <span>This is typed third.</span>
+</p>
+```
+
+"This is typed" is consistent across all sentences so it is never deleted.
+
+
+## JavaScript usage
+
+typist.js can be dynamically applied in JavaScript by passing a single element in an option object:
+
+```js
+new Typist({
+  element: document.getElementById('myelement')
+});
+```
+
+
+## typist.js options
+
+The following options can be set as HTML5 attributes or JavaScript object options:
+
+|HTML5 attribute|JS object option|description|
+|-|-|-|
+|`data-typist=""`|`element: node`|set element(s) to type|
+|`data-typist="N"`|`typist: N`|repeat typing N times. Omit to repeat forever (the default).|
+|`data-cursor="N"`|`cursor: N`|set 0 for no cursor, 1 to show cursor until typing completes, or 2 to show cursor forver (default 1)|
+|`data-delay-start="N"`|`delayStart: N`|delay typing for N milliseconds or pass -1 to delay typing until the element is scrolled into view (default 0)|
+|`data-delay-end="N"`|`delayEnd: N`|pause for N milliseconds before typing the next item (default 2000)|
+|`data-delay-type="N"`|`delayType: N`|pause for N milliseconds between each character (default 80)|
+|`data-delay-variance="N"`|`delayVariance: N`|randomly vary the typing speed by N milliseconds (default 50 - each character therefore appears at 30ms to 130ms intervals)|
+|`data-sequence="NAME"`|`delaySequence: NAME`|elements with the same NAME are typed in order - each will complete one animation before the next element is typed|
+
+The following named sequence example types each list item in turn when the first is scrolled into view:
+
+```html
+<ul>
+  <li data-typist="1" data-sequence="mylist" data-delay-start="-1">item one</li>
+  <li data-typist="1" data-sequence="mylist">item two</li>
+  <li data-typist="1" data-sequence="mylist">item three</li>
+</ul>
+```
+
+The following code segments have the same result; an item is typed three times using a delay of 300-500ms between characters once it appears in view:
+
+```html
+<p
+  data-typist="3"
+  data-delay-start="-1"
+  data-delay-type="400"
+  data-delay-variance="100">This will be typed.</p>
+```
+
+Alternatively:
+
+```html
+<p id="myelement">This will be typed.</p>
+<script>
+new Typist({
+  element: document.getElementById('myelement'),
+  typist: 3,
+  delayStart: -1,
+  delayType: 400,
+  delayVariance: 100
+});
+</script>
+```
+
+## CSS options
+
+typist.css is optional and only required for the cursor animation which can be configured as necessary.
+
+A typist class is applied to all elements being typed, so global styles can be applied if required.
+
+
+## Usage notes
+
+1. Default settings can be changed in the init variable.
+1. Older browsers (IE10 and below) show all text and do not animate.
+1. IE11 does not support data-sequence names and data-delay-start="-1" to animate when in view.
---
+++ src/js/typist.js
@@ -0,0 +1,199 @@
+// typist.js, v1.0.0
+// by Craig Buckler, @craigbuckler
+
+window.Typist = window.Typist || function(){};
+
+(function() {
+
+  'use strict';
+
+  if (!window.addEventListener || !window.requestAnimationFrame) return;
+
+  var
+    // defaults
+    init = {
+      element:        '[data-typist]',
+      cls:            'typist',
+      clsCursor:      'cursor',
+      cursor:         1,                // 0 - no cursor, 1 - show while typing, 2 - show at end
+      delayStart:     0,                // milliseconds or -N to start when in view
+      delayType:      80,
+      delayVariance:  50,
+      delayEnd:       2000,
+      viewThreshold:  1
+    },
+    // intersection observer
+    observer = (window.IntersectionObserver && window.CustomEvent) ? new IntersectionObserver(
+
+      function(entries) {
+
+        entries.forEach(function(entry) {
+
+          if (entry.intersectionRatio < init.viewThreshold) return;
+
+          // trigger event when element is in view
+          var event = new CustomEvent('typist', { detail: { element: entry.target }});
+          document.dispatchEvent(event);
+
+        });
+
+      },
+      { threshold: init.viewThreshold }
+
+    ) : null;
+
+
+  // typist object
+  var Typist = function(arg) {
+
+    var T = this;
+    arg = arg || {};
+
+    // get element
+    T.element = arg.element || null;
+    if (!T.element || T.element.nodeType !== 1 || !T.element.dataset) return;
+
+    // set defaults
+    T.text = T.getText() || ds.text.split('|');
+    if (!T.text.length) return;
+
+    var ds = T.element.dataset;
+    T.repeat = parseFloat(arg.typist || ds.typist) || Infinity;
+    T.cursor = parseFloat(arg.cursor || ds.cursor) || init.cursor;
+    T.sequence = observer ? arg.sequence || ds.sequence : null;
+
+    T.delayStart = parseFloat(arg.delayStart || ds.delayStart) || init.delayStart;
+    T.delayType = parseFloat(arg.delayType || ds.delayType) || init.delayType;
+    T.delayEnd = parseFloat(arg.delayEnd || ds.delayEnd) || init.delayEnd;
+    T.delayVariance = parseFloat(arg.delayVariance || ds.delayVariance) || init.delayVariance;
+
+    // initialise
+    T.element.innerHTML = '';
+    T.element.classList.add(init.cls);
+    T.item = 0;
+    T.char = 0;
+    T.dir = 1;
+
+    // start typing
+    if (!observer || (T.delayStart >= 0 && !T.sequence)) T.start();
+    else {
+
+      // wait until element is in view
+      observer.observe(T.element);
+
+      // element in view
+      document.addEventListener('typist', function(e) {
+
+        if (
+          (!T.sequence && e.detail.element === T.element) ||
+          document.querySelector('[data-sequence="' + T.sequence + '"]') === T.element
+        ) {
+
+          observer.unobserve(T.element);
+          T.start();
+
+        }
+      }, false);
+
+    }
+
+  };
+
+
+  // start typing
+  Typist.prototype.start = function() {
+
+    var T = this;
+
+    if (T.started) return;
+    T.started = true;
+
+    setTimeout(function() {
+      if (T.cursor > 0) T.element.classList.add(init.clsCursor);
+      T.typeText();
+    }, T.delayStart > 0 ? T.delayStart : 1);
+
+  };
+
+
+  // type text
+  Typist.prototype.typeText = function() {
+
+    var
+      T = this,
+      txt = T.text[T.item],
+      len = txt.length,
+      next = (T.item + 1) % T.text.length,
+      delay = 0, str;
+
+    T.char += T.dir;
+
+    if (!T.char || T.char >= len) T.dir *= -1;
+    if (!T.char) T.item = next;
+
+    str = txt.slice(0, T.char);
+
+    if (T.dir === -1 && T.text.length > 1 && !T.text[next].indexOf(str)) {
+      T.item = next;
+      T.dir = 1;
+    }
+
+    // typing delay
+    if (T.char >= len) {
+
+      // end of sequence reached?
+      if (!next) T.repeat--;
+      delay = T.repeat ? T.delayEnd : 0;
+      if (T.sequence) {
+        var event = new CustomEvent('typist', { detail: { sequence: T.sequence }});
+        T.sequence = null;
+        T.element.removeAttribute('data-sequence');
+        document.dispatchEvent(event);
+      }
+
+    }
+    else if (T.dir > 0) delay = Math.random() * (T.delayVariance * 2) - T.delayVariance + T.delayType;
+    else delay = T.delayType - T.delayVariance;
+
+    // show text
+    requestAnimationFrame(function() {
+
+      T.element.textContent = str;
+      if (delay) setTimeout(function() { T.typeText(); }, delay);
+      else if (T.cursor === 1) T.element.classList.remove(init.clsCursor);
+
+    });
+
+  };
+
+
+  // fetch text items to type
+  Typist.prototype.getText = function() {
+
+    var text = [], child = this.element.children, e;
+
+    for (e = 0; e < child.length; e++) addText(child[e]);
+    if (!text.length) addText(this.element);
+
+    return text.length ? text : null;
+
+    function addText(e) {
+      var t = e.textContent.trim();
+      if (t) text.push(t);
+    }
+
+  };
+
+
+  // public object
+  window.Typist = Typist;
+
+  // apply Typist to HTML elements
+  window.addEventListener('DOMContentLoaded', function() {
+
+    var typistElement = document.querySelectorAll(init.element);
+    for (var t = 0; t < typistElement.length; t++) new Typist({ element: typistElement[t] });
+
+  }, false);
+
+})();
